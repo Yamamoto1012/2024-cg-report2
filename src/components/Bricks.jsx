e5b99da6b67809669d7db9580934afbd7be69c55
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-07-18 02:36:25 +0900

    propsを受けられるようにする

--- src/components/Bricks.jsx
+++ src/components/Bricks.jsx
@@ -1,11 +1,15 @@
 import Brick from "./Brick";
 
-export default function Bricks({ userInteracted }) {
+export default function Bricks({ userInteracted, setBlocksDestroyed }) {
   let positions = [];
   for (let x = -10; x <= 10; x += 5) {
     for (let y = 5; y <= 15; y += 5) {
       positions.push([x, y, 0]);
     }
+  }
+
+  const handleBlockDestroyed = () => {
+    setBlocksDestroyed((prev) => prev + 1)
   }
 
   return (
@@ -16,6 +20,7 @@ export default function Bricks({ userInteracted }) {
           position={pos}
           userInteracted={userInteracted}
           color={index % 2 === 0 ? "red" : "blue"}
+          onBlockDestroyed={handleBlockDestroyed}
         />
       ))}
     </>
